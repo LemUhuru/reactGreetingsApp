ca18eaacf7d974c3d101a08fe812b48018e03a77
kinglemuel <pro@kinglemuel.com>
2016-08-17 13:10:49 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# React Greetings App
+
+A Vroom Challenge
+
+Create a React front-end SPA with link and form to submit and return a response.
+Form request should output to console and response should render to page.
---
+++ public/assets/components.js
@@ -0,0 +1,30 @@
+class GreetingContainer extends React.component {
+  constructor() {
+    super();
+
+    this.state = {
+      greetings: [];
+    };
+  }
+
+  componentWillMount() {
+    this._getGreetings();
+  }
+
+  render() {
+    const greetings = this.get
+  }
+}
+class GreetingForm extends React.component {
+
+}
+class Greeting extends React.component {
+
+}
+
+jQuery(function() {
+  ReactDOM.render(
+    <GreetingForm />,
+    document.getElementById('greeting-form')
+  );
+})
